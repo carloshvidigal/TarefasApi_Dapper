8b52470c673f6c8d3d78d418c2772682cdb31d91
carloshvidigal <chmovi@hotmail.com>
2026-01-30 11:23:12 -0300

    Ajustes Program.cs e csproj

--- TarefasApi_Dapper/Program.cs
+++ TarefasApi_Dapper/Program.cs
@@ -1,6 +1,12 @@
+using TarefasApi.Extensions;
+using TarefasApi_Dapper.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddPersistence();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapTarefasEndpoints();
 
 app.Run();
