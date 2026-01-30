5065dcf44b5db00dc760a7e0b2e388b8afaf0f9c
carloshvidigal <chmovi@hotmail.com>
2026-01-30 13:15:29 -0300

    Implementar endpoints Put e Delete da minimal API usando dapper

--- TarefasApi_Dapper/Endpoints/TarefasEndpoints.cs
+++ TarefasApi_Dapper/Endpoints/TarefasEndpoints.cs
@@ -39,6 +39,26 @@ namespace TarefasApi_Dapper.Endpoints
                 return Results.Created($"/tarefas/{id}", Tarefa);
             });
 
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(Tarefa);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+
+                var deleted = con.Get<Tarefa>(id);
+
+                if (deleted is null)
+                    return Results.NotFound();
+
+                con.Delete(deleted);
+                return Results.Ok(deleted);
+            });
+
         }
     }
 }
