c40164e0602a6ea591840dec7f93fccd30ae1a9f
carloshvidigal <chmovi@hotmail.com>
2026-01-30 11:23:29 -0300

    Criara TarefasEndpoint e método GetAll

---
+++ TarefasApi_Dapper/Endpoints/TarefasEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using Dapper.Contrib.Extensions;
+using TarefasApi_Dapper.Data;
+using static TarefasApi_Dapper.Data.TarefaContext;
+
+namespace TarefasApi_Dapper.Endpoints
+{
+    public static class TarefasEndpoints
+    {
+        public static void MapTarefasEndpoints(this WebApplication app)
+        {
+            app.MapGet("/", () => $"Bem-Vindo a API Tarefas {DateTime.Now}");
+
+            app.MapGet("/tarefas", async (GetConnection connectionGetter) =>
+            {
+                using var con = await connectionGetter();
+                var tarefas = con.GetAll<Tarefa>().ToList();
+
+                if (tarefas is null)
+                    return Results.NotFound();
+
+                return Results.Ok(tarefas);
+            });
+
+        }
+    }
+}
