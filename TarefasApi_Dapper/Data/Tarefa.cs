1397080e2c553d9d9373e952f6b78d1782b3c7ed
carloshvidigal <chmovi@hotmail.com>
2026-01-30 11:22:40 -0300

    Implementar Data

---
+++ TarefasApi_Dapper/Data/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TarefasApi_Dapper.Data
+{
+    [Table("Tarefas")]
+    public record Tarefa(int Id, string Atividade, string Status);
+
+
+}
---
+++ TarefasApi_Dapper/Data/TarefaContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace TarefasApi_Dapper.Data
+{
+    public class TarefaContext
+    {
+        public delegate Task<IDbConnection> GetConnection(); 
+    }
+}
